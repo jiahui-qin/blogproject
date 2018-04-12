24fbf4a19b9360ca52b3f157882f6920dfdf5346
saber <feverqjh@outlook.com>
2018-04-12 23:34:31 +0800

    form, alert

---
+++ blog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class Logfrom(forms.Form):
+
+
+
+class testrecordform(forms.Form):
+
+
+class curdeexform(forms.Form):
+    pass
+
+class bactform(forms.Form):
+    pass
--- blog/views.py
+++ blog/views.py
@@ -9,50 +9,49 @@ from django.db.models import Q
 from django.utils import timezone
 
 @login_required
-def index(request):
+def index(request, msg = -1):
     tests = testrecord.objects.all()
 #    #return HttpResponseRedirect(reverse(NAME_OF_PROFILE_VIEW, args=[request.user.username]))
-    return render(request, 'blog/tets.html', context = {'tests' : tests} )
+    return render(request, 'blog/tets.html', context = {'tests' : tests, 'msg' : msg} )
 
 def upload(request):
     if request.method == "GET":
-        testtype = request.GET.get('testtype') #或者改成两个可选项？粗提物或者化合物
-        boxnumber = request.GET.get('boxnumber') #盒序号
-        samplestart = request.GET.get('samplestart') #样品起序号
-        sampleend = request.GET.get('sampleend') #样品终序号
-        samplename = request.GET.get('samplename') #样品名
-        samplenum = int(sampleend) - int(samplestart) + 1 #样品数量
-        solvent = request.GET.get('solvent') #溶剂
-        mass = request.GET.get('mass')#重量，单位miug
-        volume = request.GET.get('volume') #体积单位niuL
-        concentration = request.GET.get('concentration') #浓度 单位(mg/mL)
-        testconcentration = request.GET.get('testconcentration') #测试浓度 单位(miug/mL)
-        department = request.GET.get('department') #测样单位
-        #sendtime = request.GET.get('sendtime') #送样时间
-        comment =request.GET.get('comment')
-        info = {
-            'testtype' : testtype, 
-            'boxnumber' : boxnumber, 
-            'samplestart' : samplestart, 
-            'sampleend' : sampleend, 
-            'samplenum' : samplenum,
-            'samplename' : samplename, 
-            'solvent' : solvent, 
-            'mass' : mass, 
-            'volume' : volume, 
-            'concentration' : concentration, 
-            'testconcentration' : testconcentration, 
-            'department' : department,  
-            'comment' : comment,
-            'provider': request.user
-        }
-        status = 0
         try:
+            testtype = request.GET.get('testtype') #或者改成两个可选项？粗提物或者化合物
+            boxnumber = request.GET.get('boxnumber') #盒序号
+            samplestart = request.GET.get('samplestart') #样品起序号
+            sampleend = request.GET.get('sampleend') #样品终序号
+            samplename = request.GET.get('samplename') #样品名
+            samplenum = int(sampleend) - int(samplestart) + 1 #样品数量
+            solvent = request.GET.get('solvent') #溶剂
+            mass = request.GET.get('mass')#重量，单位miug
+            volume = request.GET.get('volume') #体积单位niuL
+            concentration = request.GET.get('concentration') #浓度 单位(mg/mL)
+            testconcentration = request.GET.get('testconcentration') #测试浓度 单位(miug/mL)
+            department = request.GET.get('department') #测样单位
+            #sendtime = request.GET.get('sendtime') #送样时间
+            comment =request.GET.get('comment')
+            info = {
+                'testtype' : testtype, 
+                'boxnumber' : boxnumber, 
+                'samplestart' : samplestart, 
+                'sampleend' : sampleend, 
+                'samplenum' : samplenum,
+                'samplename' : samplename, 
+                'solvent' : solvent, 
+                'mass' : mass, 
+                'volume' : volume, 
+                'concentration' : concentration, 
+                'testconcentration' : testconcentration, 
+                'department' : department,  
+                'comment' : comment,
+                'provider': request.user
+            }        
             testrecord.objects.create(**info)
-            return index(request)
+            return index(request, msg = 0) #msg = 0代表正常插入
         except:
             print(222)
-            return index(request)
+            return index(request, msg = 1) #msg = 1 代表插入失败
 
 @login_required
 def altermanage(request):
