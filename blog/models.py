6e4439c7fe547790a1c88c67cdbd29025f7c7cc6
saber <feverqjh@outlook.com>
2018-04-14 14:29:01 +0800

    4/14完善了models

--- blog/models.py
+++ blog/models.py
@@ -51,7 +51,7 @@ class crudeex(models.Model):
     entertime = models.DateTimeField() #入库时间
     entervol = models.FloatField() #入库体积
     entercol =  models.FloatField() #入库浓度
-    tetscol = models.FloatField() #测试浓度
+    testcol = models.FloatField() #测试浓度
     testvol = models.FloatField() #测试体积
     activecol = models.FloatField() #活性浓度
     solvent = models.CharField(max_length = 100) #溶剂
@@ -71,9 +71,50 @@ class bact(models.Model):
     sourcenum = models.CharField(max_length = 50) #平台资源编号
     genus = models.CharField(max_length = 200) #属名
     species = models.CharField(max_length = 200) #种名
-    chinesename = models.CharField(max_length = 200) #中文名
-    othersourcenum = models.CharField(max_length = 100) #其它平台资源编号
-    history = models.CharField(max_length = 100) #来源历史
+    chinesename = models.CharField(max_length = 200, blank = True) #中文名
+    othersourcenum = models.CharField(max_length = 100, blank = True) #其它平台资源编号
+    history = models.CharField(max_length = 100, blank = True) #来源历史
     storetimr = models.DateTimeField() #收藏时间
+    orinumber = models.CharField(max_length = 20, blank = True)#原始资源编号
+    mainuse = models.CharField(max_length = 200, blank = True)#主要用途
+    danger = models.CharField(max_length = 50, blank = True)#生物危害程度
+    hostname = models.CharField(max_length = 100, blank = True)#寄主名称
+    ill = models.CharField(max_length = 100, blank = True) #致病名称
+    tranroute = models.CharField(max_length = 100, blank = True) #传播途径
+    media = models.CharField(max_length = 50, blank = True) #培养基编号
+    mediatem = models.CharField(max_length = 10, blank = True) #培养基温度
+    gene = models.CharField(max_length = 50, blank = True) # 基因元器件
+    recadd = models.CharField(max_length = 100, blank = True)#记录地址
+    shareme = models.CharField(max_length = 100, blank = True)#共享方式
+    getmet = models.CharField(max_length = 100, blank = True) #获取途径
+    callme = models.CharField(max_length = 30, blank = True)#联系方式
+    mainkey = models.CharField(max_length = 100, blank = True)#源数据主键
+    prod = models.CharField(max_length = 30, blank = True)#提供人
+    didv = models.CharField(max_length = 30, blank = True)#分离人
+    judge = models.CharField(max_length = 30, blank = True)#鉴定人
+    genenumber = models.CharField(max_length = 50, blank = True)#基因序列注册号
+
     def __str__(self):
         return self.bactnumber + '|' + self.chinesename
+
+
+class cpd(models.Model):
+    cpdnumber = models.CharField(max_length = 20)#化合物编号
+    frombact = models.CharField(max_length = 20)#来源菌株
+    mass = models.FloatField() #精确质量数
+    stru = models.CharField(max_length = 50)#分子式
+    nmr = models.CharField(max_length = 100)#nmr数据
+    ms = models.CharField(max_length = 100)#ms数据
+    rot = models.CharField(max_length = 100)#旋光数据
+    red = models.CharField(max_length = 100)#红外数据
+    blue = models.CharField(max_length = 100)#紫外数据2
+    media = models.CharField(max_length = 50)#培养基编号
+    depa = models.CharField(max_length = 100)#活性送测范围
+    spc = models.CharField(max_length = 100)#活性种类
+    time = models.DateTimeField()#活性送测时间
+    resu = models.CharField(max_length = 100)#活性结果
+    prov = models.CharField(max_length = 100)#提供人
+    dtime = models.DateTimeField()#入库时间
+    
+    def __str__(self):
+        return self.cpdnumber + '|' + self.stru
