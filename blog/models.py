353ed8ced9c226b2461153131f1ae2bd6b54f5cd
saber <feverqjh@outlook.com>
2018-05-03 17:30:26 +0800

    11

--- blog/models.py
+++ blog/models.py
@@ -37,35 +37,8 @@ class testrecord(models.Model):
     def __str__(self):
         return self.samplename
 
-class crudeex(models.Model):
-    mcccnumber = models.CharField(max_length = 50) #mccc编号
-    genus = models.CharField(max_length = 200) #属名
-    species = models.CharField(max_length = 200) #种名
-    chinesename = models.CharField(max_length = 200) #中文名
-    media = models.CharField(max_length = 200) #培养基
-    medianumber = models.CharField(max_length = 50) #培养基编号
-    mass = models.FloatField() #重量，单位mg
-    stockmass = models.FloatField() #库存量，单位mg
-    cointermass = models.FloatField() #样品瓶重量，单位miug
-    entertime = models.DateTimeField(auto_now_add=True, editable = True) #入库时间
-    entervol = models.FloatField() #入库体积
-    entercol =  models.FloatField() #入库浓度
-    testcol = models.FloatField() #测试浓度
-    testvol = models.FloatField() #测试体积
-    activecol = models.FloatField() #活性浓度
-    solvent = models.CharField(max_length = 100) #溶剂
-    culture = models.CharField(max_length = 500, blank = True) #培养条件
-    exrmethod = models.CharField(max_length = 500, blank = True) #提取方法
-    provider = models.ForeignKey(User) #提供人
-    department = models.CharField(max_length=100) #测样单位
-    sendtime = models.DateTimeField(auto_now_add=True,editable = True) #送样时间
-    activeresult = models.CharField(max_length = 500, blank = True) #活性检测结果
-    comment = models.CharField(max_length = 500, blank = True)
-    def __str__(self):
-        return self.crudenumber + '|' + self.chinesename
 
-
-class bact(models.Model):
+class bact(models.Model):  #菌株
     bactnumber = models.CharField(max_length = 50) #菌株保藏编号
     sourcenum = models.CharField(max_length = 50) #平台资源编号
     genus = models.CharField(max_length = 200) #属名
@@ -96,8 +69,40 @@ class bact(models.Model):
     def __str__(self):
         return self.bactnumber + '|' + self.chinesename
 
+class crudeex(models.Model): ##粗提物
+    frombact = models.ForeignKey(bact)
+    mcccnumber = models.CharField(max_length = 50) #mccc编号
+    genus = models.CharField(max_length = 200) #属名
+    species = models.CharField(max_length = 200) #种名
+    chinesename = models.CharField(max_length = 200) #中文名
+    media = models.CharField(max_length = 200) #培养基
+    medianumber = models.CharField(max_length = 50) #培养基编号
+    mass = models.FloatField() #重量，单位mg
+    stockmass = models.FloatField() #库存量，单位mg
+    cointermass = models.FloatField() #样品瓶重量，单位miug
+    entertime = models.DateTimeField(auto_now_add=True, editable = True) #入库时间
+    entervol = models.FloatField() #入库体积
+    entercol =  models.FloatField() #入库浓度
+    testcol = models.FloatField() #测试浓度
+    testvol = models.FloatField() #测试体积
+    activecol = models.FloatField() #活性浓度
+    solvent = models.CharField(max_length = 100) #溶剂
+    culture = models.CharField(max_length = 500, blank = True) #培养条件
+    exrmethod = models.CharField(max_length = 500, blank = True) #提取方法
+    provider = models.ForeignKey(User) #提供人
+    department = models.CharField(max_length=100) #测样单位
+    sendtime = models.DateTimeField() #送样时间
+    activeresult = models.CharField(max_length = 500, blank = True) #活性检测结果
+    comment = models.CharField(max_length = 500, blank = True)
+    
+    
+    #def __str__(self):
+    #    return self.crudenumber + '|' + self.chinesename
 
-class cpd(models.Model):
+
+
+
+class cpd(models.Model): ##化合物
     cpdnumber = models.CharField(max_length = 20)#化合物编号
     frombact = models.CharField(max_length = 20)#来源菌株
     mass = models.FloatField() #精确质量数
